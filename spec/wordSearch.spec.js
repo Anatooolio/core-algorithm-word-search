754197a2b3cdc952cb83c6cbddd414060462f394
Anatooolio <margach1465@gmail.com>
2024-11-20 21:44:05 +0300

    only string search

--- spec/wordSearch.spec.js
+++ spec/wordSearch.spec.js
@@ -1,20 +1,39 @@
-describe ('searchStraightLineInclude', () => {
-    it('', () => {
-        expect()
-    })
+const {
+	searchStraightLineInclude,
+	searchSnakingInclude,
+} = require('../wordSearch')
 
-    it('', () => {
-        expect()
-    })
+let puzzle
+
+beforeEach(() => {
+	puzzle = [
+		['a', 'k', 'f', 'o', 'x', 'e', 's'],
+		['s', 'o', 'a', 'w', 'a', 'h', 'p'],
+		['i', 't', 'c', 'k', 'e', 't', 'n'],
+		['o', 't', 's', 'd', 'h', 'o', 'h'],
+		['s', 'e', 'x', 'g', 's', 't', 'a'],
+		['u', 'r', 'p', 'i', 'w', 'e', 'u'],
+		['z', 's', 'b', 'n', 'u', 'i', 'r'],
+	]
+    
 })
 
+describe('Найти все слова в линию', () => {
+	it('Выводит true если есть слово в строке', () => {
+		expect(searchStraightLineInclude('foxes', puzzle)).toBe(true)
+	})
 
-describe ('searchSnakingInclude', () => {
-    it('', () => {
-        expect()
-    })
+	it('Выводит false если слова нет', () => {
+		expect(searchStraightLineInclude('akoxes', puzzle)).toBe('false')
+	})
+})
 
-    it('', () => {
-        expect()
-    })
+describe('Найти все слова путем змейки', () => {
+	it('', () => {
+		expect()
+	})
+
+	it('', () => {
+		expect()
+	})
 })
--- wordSearch.js
+++ wordSearch.js
@@ -1,7 +1,34 @@
-function searchStraightLineInclude(word, puzzle){
+const puzzle = [
+	['a', 'k', 'f', 'o', 'x', 'e', 's'],
+	['s', 'o', 'a', 'w', 'a', 'h', 'p'],
+	['i', 't', 'c', 'k', 'e', 't', 'n'],
+	['o', 't', 's', 'd', 'h', 'o', 'h'],
+	['s', 'e', 'x', 'g', 's', 't', 'a'],
+	['u', 'r', 'p', 'i', 'w', 'e', 'u'],
+	['z', 's', 'b', 'n', 'u', 'i', 'r'],
+]
+const word = 'foxes'
+console.table(puzzle)
+// console.log(puzzle[i][i]) Вызвало буквы по диагонали
+// console.log(puzzle[1][0]);
 
+function searchStraightLineInclude(word, puzzle) {
+	// Проверка горизонтальных линий
+	for (let i = 0; i < puzzle.length; i += 1) {
+		if (
+			puzzle[i].join('').includes(word) ||
+			puzzle[i].reverse().join('').includes(word)
+		) {
+			return true
+		}
+	}
+	return false
 }
+console.log(searchStraightLineInclude(word, puzzle))
 
-function searchSnakingInclude(word, puzzle){
+// function searchSnakingInclude(word, puzzle) {}
 
-}
+// module.export = {
+// 	searchStraightLineInclude,
+// 	searchSnakingInclude,
+// }
